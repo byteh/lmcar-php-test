ced7a29ff030e55de0620d52e79629b2c6f16b2f
hanyubin <hanyubin@luojilab.com>
2022-12-20 10:48:03 +0800

    log4think

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -1,17 +1,94 @@
 <?php
 
 namespace App\Service;
+use think\facade\Log;
+
+// 策略接口 约定策略的行为
+interface LogStrategy {
+    public function dealMessage($message);
+}
+
+// TYPE_LOG4PHP
+class Log4Strategy implements LogStrategy {
+
+    private $logger;
+
+    public function __construct()
+    {
+        $this->logger = \Logger::getLogger("Log");
+    }
+
+    public function dealMessage($message){
+        return $message;
+    }
+
+    public function info($message = '')
+    {
+        $this->logger->info($message);
+    }
+
+    public function debug($message = '')
+    {
+        $this->logger->debug($message);
+    }
+
+    public function error($message = '')
+    {
+        $this->logger->error($message);
+    }
+}
+
+// TYPE_ThinkLog
+class ThinkLogStrategy implements LogStrategy {
+
+    public function __construct()
+    {
+        Log::init([
+            'default'	=>	'file',
+            'channels'	=>	[
+                'file'	=>	[
+                    'type'	=>	'file',
+                    'path'	=>	'./logs/',
+                ],
+            ],
+        ]);
+    }
+
+    public function dealMessage($message){
+        return strtoupper($message);
+    }
+
+    public function info($message = '')
+    {
+        Log::info($this->dealMessage($message));
+    }
+
+    public function debug($message = '')
+    {
+        Log::debug($this->dealMessage($message));
+    }
+
+    public function error($message = '')
+    {
+        Log::error($this->dealMessage($message));
+    }
+}
+
 
 class AppLogger
 {
     const TYPE_LOG4PHP = 'log4php';
+    const TYPE_THINKLOG = 'thinkLog';
 
     private $logger;
 
     public function __construct($type = self::TYPE_LOG4PHP)
     {
         if ($type == self::TYPE_LOG4PHP) {
-            $this->logger = \Logger::getLogger("Log");
+//            $this->logger = \Logger::getLogger("Log");
+            $this->logger = new Log4Strategy();
+        } else {
+            $this->logger = new ThinkLogStrategy();
         }
     }
 
--- tests/Service/AppLoggerTest.php
+++ tests/Service/AppLoggerTest.php
@@ -13,7 +13,18 @@ class AppLoggerTest extends TestCase
 
     public function testInfoLog()
     {
+        $msg = 'This is info log message';
+
         $logger = new AppLogger('log4php');
-        $logger->info('This is info log message');
+        $logger->info($msg);
+        $logger->debug($msg);
+        $logger->error($msg);
+
+        $loggerT = new AppLogger('thinkLog');
+        $loggerT->info($msg);
+        $loggerT->debug($msg);
+        $loggerT->error($msg);
+
+        $this->assertEquals('This is info log message', $msg);
     }
 }
